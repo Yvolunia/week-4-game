f428f1fc3fa21f29cebd6ae46e38f9ca43bbe120
Yvonne Reichenbach <yvonnereichenbach@Yvonnes-MacBook-Pro.local>
2017-12-12 10:11:53 -0500

    Initial Folders and Files

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+
+
+  // Game code goes here
+
+  // Show the player their progress
+
+  // Take input from the player
+
+  // Update answerArray and remainingLetters for every correct guess
+
+}
+
+
+var answerArray = [];
+
+for (var i = 0; i < word.length; i++) {
+
+    answerArray[i] = "_";
+
+  }
+
+  var remainingLetters = word.length;
